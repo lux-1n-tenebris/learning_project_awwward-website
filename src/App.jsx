a2b595eb0ec0f0bc9d9af2c163ab14b55f472f22
Stefan Radosavljevic <s.radosavljevic669@gmail.com>
2025-09-24 12:56:17 +0200

    adding navbar functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
 import About from './components/About.jsx';
 
@@ -6,6 +7,7 @@ import About from './components/About.jsx';
 const App = () => {
     return (
         <main className="relative min-h-screen w-screen overflow-x-hidden">
+            <Navbar/>
             <Hero/>
             <About/>
         </main>
