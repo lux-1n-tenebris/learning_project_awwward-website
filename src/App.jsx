946fc162bc33f9f9fb6bbbbf7e53e2c60c928076
Stefan Radosavljevic <s.radosavljevic669@gmail.com>
2025-09-24 14:23:06 +0200

    adding story section

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
 import About from './components/About.jsx';
 import Features from './components/Features.jsx';
-
+import Story from './components/Story.jsx';
 
 const App = () => {
     return (
@@ -12,6 +12,7 @@ const App = () => {
             <Hero/>
             <About/>
             <Features />
+            <Story />
         </main>
     )
 }
